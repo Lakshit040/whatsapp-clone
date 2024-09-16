91020471ff542486c6daf7be7245cea291ae6b34
Lakshit Sharma <lakshit.sharma@lakshit.sharma-C02DV2P8MD6N>
2024-09-16 15:17:11 +0530

    minor fix

--- src/utils.ts
+++ src/utils.ts
@@ -2,17 +2,17 @@ import { format } from 'date-fns';
 import _ from 'lodash';
 import { v4 } from 'uuid';
 
+const enum Format {
+  DateTime = 'yyyy-MM-dd HH:mm',
+  Time = 'HH:mm',
+}
+
 export const PROFILE_IMG =
   'https://fastly.picsum.photos/id/297/200/300.jpg?hmac=SF0Y51mRP7i6CoLBIuliqQwDIUJNyf63_r3xhamVSLE';
 
 export const enum Mode {
   Compact,
   Spacious,
-}
-
-export const enum Format {
-  DateTime = 'yyyy-MM-dd HH:mm',
-  Time = 'HH:mm',
 }
 
 export type Message = {
