efa5036018e45998ca5474c54bfc940286ebb11b
Lakshit Sharma <lakshit.sharma@lakshit.sharma-C02DV2P8MD6N>
2024-09-20 10:49:51 +0530

    minor bug fixes

--- src/extra.tsx
+++ src/extra.tsx
@@ -4,29 +4,7 @@ type DependencyList = ReadonlyArray<any>;
 type Callback = () => void;
 
 const deepCompare = (obj1: any, obj2: any): boolean => {
-  if (obj1 === obj2) return true;
-
-  if (
-    typeof obj1 !== 'object' ||
-    obj1 === null ||
-    typeof obj2 !== 'object' ||
-    obj2 === null
-  ) {
-    return false;
-  }
-
-  const keys1 = Object.keys(obj1);
-  const keys2 = Object.keys(obj2);
-
-  if (keys1.length !== keys2.length) return false;
-
-  for (const key of keys1) {
-    if (!keys2.includes(key) || !deepCompare(obj1[key], obj2[key])) {
-      return false;
-    }
-  }
-
-  return true;
+  return JSON.stringify(obj1) === JSON.stringify(obj2);
 };
 
 // to mimic constructor in class component
@@ -79,8 +57,8 @@ export const useDidReceiveProps = <T extends Record<string, any>>(
 };
 
 // to hold the previous value of the variable
-export const usePrevious = <T,>(value: T): T | undefined => {
-  const ref = useRef<T | undefined>(undefined);
+export const usePrevious = <T,>(value: T, initialValue?: T): T | undefined => {
+  const ref = useRef<T | undefined>(initialValue);
 
   useEffect(() => {
     ref.current = value;
